03a2124b36403358f705a8141a52edc748e81be0
towakey <kyosgame@gmail.com>
2022-09-17 14:52:57 +0900

    venderからフォーマットをコピー

--- resources/lang/ja/auth.php
+++ resources/lang/ja/auth.php
@@ -4,16 +4,80 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | 認証言語行
+    | Authentication Language Lines
     |--------------------------------------------------------------------------
     |
-    | 以下の言語行は認証時にユーザーに対し表示する必要のある
-    | 様々なメッセージです。アプリケーションの必要に合わせ
-    | 自由にこれらの言語行を変更してください。
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
     |
     */
 
-    'failed' => 'ログイン情報が登録されていません。',
-    'throttle' => 'ログインに続けて失敗しています。:seconds秒後に再度お試しください。',
+    'failed'   => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
+    'full_name'        => 'Full Name',
+    'email'            => 'Email',
+    'password'         => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'remember_me'      => 'Remember Me',
+    'sign_in'          => 'Sign In',
+    'sign_out'         => 'Sign out',
+    'register'         => 'Register',
+
+    'login' => [
+        'title'               => 'Sign in to start your session',
+        'forgot_password'     => 'I forgot my password',
+        'register_membership' => 'Register a new membership',
+    ],
+
+    'registration' => [
+        'title'           => 'Register a new membership',
+        'i_agree'         => 'I agree to',
+        'terms'           => 'the terms',
+        'have_membership' => 'I already have a membership',
+    ],
+
+    'forgot_password' => [
+        'title'          => 'Enter Email to reset password',
+        'send_pwd_reset' => 'Send Password Reset Link',
+    ],
+
+    'reset_password' => [
+        'title'         => 'Reset your password',
+        'reset_pwd_btn' => 'Reset Password',
+    ],
+
+    'confirm_passwords' => [
+        'title'                => 'Please confirm your password before continuing.',
+        'forgot_your_password' => 'Forgot Your Password?',
+    ],
+
+    'verify_email' => [
+        'title'       => 'Verify Your Email Address',
+        'success'     => 'A fresh verification link has been sent to your email address',
+        'notice'      => 'Before proceeding, please check your email for a verification link.If you did not receive the email,',
+        'another_req' => 'click here to request another',
+    ],
+
+    'emails' => [
+        'password' => [
+            'reset_link' => 'Click here to reset your password',
+        ],
+    ],
+
+    'app' => [
+        'member_since' => 'Member since',
+        'messages'     => 'Messages',
+        'settings'     => 'Settings',
+        'lock_account' => 'Lock Account',
+        'profile'      => 'Profile',
+        'online'       => 'Online',
+        'search'       => 'Search',
+        'create'       => 'Create',
+        'export'       => 'Export',
+        'print'        => 'Print',
+        'reset'        => 'Reset',
+        'reload'       => 'Reload',
+    ],
 ];
