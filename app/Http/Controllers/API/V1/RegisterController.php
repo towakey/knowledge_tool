88e24bc283efe2260bd76e0a4906ea4b9f791610
towakey <kyosgame@gmail.com>
2022-09-25 22:00:39 +0900

    クラス定義修正

--- app/Http/Controllers/API/V1/RegisterController.php
+++ app/Http/Controllers/API/V1/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class RegisterController extends Controller
+class RegisterController extends \App\Http\Controllers\Auth\RegisterController
 {
     //
     /**
